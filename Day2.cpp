603f258a314fa997ecd6c4b20df1cb1c40fa87ce
Vivek Akheniya <116439072+vivekakheniya@users.noreply.github.com>
2024-02-02 20:00:21 +0530

    Add files via upload
    Day 2 of Challenge

---
+++ Day2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int age;
+    string name;
+    cout<<"Please enter your Name and Age "<< endl;
+    cout<<"1. Name: ";
+    cin >> name;
+    cout<<endl;
+    cout<<"2. Age: ";
+    cin >> age;
+    cout<<endl;
+    cout<<"Thanks a lot for entering your Age: "<< age;
+    return 0;
+}
